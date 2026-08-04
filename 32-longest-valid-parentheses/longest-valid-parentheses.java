5e9f141eb788e9b5326fac5704c8a7de26bc8611
Harish <106366627+Harishnagenthiran@users.noreply.github.com>
2026-08-05 00:07:08 +0530

    Time: 6 ms (14.87%) | Memory: 46.2 MB (83.04%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        int left = -1;
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+                st.push(i);
+            } else {
+
+                if (st.isEmpty()) {
+                    left = i;
+                } else {
+                    st.pop();
+
+                    if (st.isEmpty()) {
+                        max = Math.max(max, i - left);
+                    } else {
+                        max = Math.max(max, i - st.peek());
+                    }
+                }
+            }
+        }
+
+        return max;
+    }
+}
